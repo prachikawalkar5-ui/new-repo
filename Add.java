aa05e634bf25bf481f4bf1eeb34a388d98989c7a
prachikawalkar5-ui <prachikawalkar5@gmail.com>
2026-03-02 10:23:04 +0530

    Fix multiplication variable to use correct operation

--- Add.java
+++ Add.java
@@ -4,9 +4,9 @@ public class Add {
         int num2 = 20;
 
         int sum = num1 + num2;
-        int mul= num1 + num2;
+   
 
         System.out.println("Sum of two numbers: " + sum);
-         System.out.println("Sum of two numbers: " + mul);
+         
     }
 }
